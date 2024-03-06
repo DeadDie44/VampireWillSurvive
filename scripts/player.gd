dcd5ca6d7de0f0a7a757d98262e6067cdf211a91
deaddie44 <deaddie44@gmail.com>
2024-03-06 18:49:23 +0300

    Updated player movement, Attached camera to the player, added some sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,44 @@
 extends CharacterBody2D
 
+const ACCELERATION = 96
+const MAX_SPEED  = 64
+const ROLL_SPEED = 100
+const FRICTION = 244
 
-@export var speed = 100
+enum {
+	MOVE,
+	ATTACK
+}
 
+var state = MOVE
+var speed = 0
 
+func _ready():
+	pass
 
-func get_input():
+func _process(delta):
+	match state:
+		MOVE:
+			move(delta)
 
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#Esc to quit game	
+	if Input.is_action_just_pressed("ui_text_clear_carets_and_selection"):
+		get_tree().quit()
 
-	velocity = input_direction * speed
-
-
-
-func _physics_process(_delta):
-
-	get_input()
-
+func _physics_process(delta):
 	move_and_slide()
+
+func move(delta):
+	var x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	var input_vector = Vector2 (x,y).normalized()
+	
+	if input_vector != Vector2.ZERO:
+		speed += ACCELERATION * delta
+		if speed > MAX_SPEED:
+			speed = MAX_SPEED
+		velocity = input_vector * speed
+	else:
+		speed = 0
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
